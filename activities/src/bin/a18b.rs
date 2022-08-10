57bbdbc60f3adc183acea839cdc4274a324e705f
Jeffery Weiler <jeff@landistechnologies.com>
2022-08-10 13:01:52 -0400

    more matches

--- activities/src/bin/a18b.rs
+++ activities/src/bin/a18b.rs
@@ -22,4 +22,49 @@
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
 
-fn main() {}
+enum EmployeeType {
+    Maintenance,
+    Marketing,
+    Manager,
+    LineSupervisor,
+    KitchenStaff,
+    AssemblyTech
+}
+
+struct Employee {
+    employed: bool,
+    employee_type: EmployeeType
+}
+
+fn can_access_building(employee: Employee) -> Result<(), String> {
+
+    match employee.employed {
+        false => return Err("terminated cannot access".to_owned()),
+        _ => ()
+    }
+
+    match employee.employee_type {
+        EmployeeType::Maintenance => Ok(()),
+        EmployeeType::Manager => Ok(()),
+        EmployeeType::Marketing => Ok(()),
+        _ => return Err("no access allowed".to_owned())
+        }
+}
+
+fn print_access(employee: Employee) -> Result<(), String> {
+    let access = can_access_building(employee)?;
+    println!("access ok");
+    Ok(())
+}
+
+fn main() {
+    let employee = Employee{
+        employed: true,
+        employee_type: EmployeeType::KitchenStaff
+    };
+    let result = print_access(employee);
+    match result {
+        Err(e) => println!("access denied: {:?}", e),
+        _ => () 
+    }
+}
