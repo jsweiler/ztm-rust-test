67e267ce098ea896eb0f8c6e6b557e050b732e75
Jeff <jeffweiler@outlook.com>
2022-08-10 20:21:50 -0400

    initial hashmap

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -12,7 +12,25 @@
 //
 // Notes:
 // * Use a HashMap for the furniture store stock
+use std::{collections::HashMap};
 
 fn main() {
-    
+    let mut items = HashMap::new();
+    items.insert("chairs", 5);
+    items.insert("beds", 3);
+    items.insert("tables", 2);
+    items.insert("couches", 0);
+
+    let mut total_count: i32 = 0;
+    for (name, &val) in items.iter() {
+        total_count = total_count + val;
+        let stock_count = if val == 0 {
+            "out of stock".to_owned()
+        } else {
+            format!("{:?}", val)
+        };
+        println!("item={:?}, stock={:?}", name, stock_count);
+    }
+
+    println!("total stock count: {:?}", total_count);
 }
