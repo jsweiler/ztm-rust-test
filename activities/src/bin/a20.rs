a383a181442254f6292a69c04f4a9f118935dff3
Jeff <jeffweiler@outlook.com>
2022-10-15 10:43:03 -0400

    get done fighting rust over str and String

--- activities/src/bin/a20.rs
+++ activities/src/bin/a20.rs
@@ -23,4 +23,54 @@
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
 
-fn main() {}
+use std::io;
+
+
+#[derive(Debug)]
+enum PowerState{
+    Off,
+    Sleep,
+    Reboot,
+    Shutdown,
+    Hibernate
+}
+
+impl PowerState {
+    fn new(state: &str) -> Option<PowerState> {
+        let new_state = state.trim();
+        let lowercase = new_state.to_lowercase();
+        let lowercase_str = lowercase.as_str();
+        match lowercase_str {
+            "off" => Some(PowerState::Off),
+            "sleep" => Some(PowerState::Sleep), 
+            "reboot" => Some(PowerState::Reboot),
+            "shutdown" => Some(PowerState::Shutdown),
+            "hibernate" =>Some(PowerState::Hibernate),
+            _ => None
+        }
+    }
+}
+
+fn print_power_action(state: PowerState) {
+    use PowerState::*;
+    match state {
+        Off => println!("turning off"),
+        Sleep => println!("sleeping"),
+        Reboot => println!("rebooting"),
+        Shutdown => println!("shutting down"),
+        Hibernate => println!("hibernating")
+    }
+}
+
+fn main() {
+    let mut buffer = String::new();
+    let user_input = io::stdin().read_line(&mut buffer);
+    if user_input.is_ok() {
+        match PowerState::new(&buffer) {
+            Some(state) => print_power_action(state),
+            None => println!("invalid state")
+        }
+    } else {
+        println!("error getting input");
+    }
+}
